b5423daf49c018e6a1465ea880dea6e268631a30
mariusToba <mariustoba@protonmail.com>
2019-07-20 22:55:20 +0300

    Completed the ReactJS tic tac toe game

--- src/Game.js
+++ src/Game.js
@@ -28,23 +28,37 @@ class Game extends React.Component {
             history: [{
                 squares: Array(9).fill(null)
             }],
+            setpNumber: 0,
             xIsNext: true
         }
     }
 
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.setpNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) return;
         squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({ history: history.concat([{ squares: squares }]), xIsNext: !this.state.xIsNext })
+        this.setState({ history: history.concat([{ squares: squares }]), setpNumber: history.length, xIsNext: !this.state.xIsNext })
+    }
+
+    jumpTo(step) {
+        this.setState({
+            setpNumber: step,
+            isNext: (step % 2) === 0,
+        })
     }
 
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1]
+        const current = history[this.state.setpNumber]
         const winner = calculateWinner(current.squares)
+
+        const moves = history.map((step, move) => {
+            const desc = move ? "Go to move #" + move : "Go to game start";
+            return (<li key={move}><button className='historyButtons' onClick={() => this.jumpTo(move)}>{desc}</button></li>)
+        })
+
         let status;
         if (winner) {
             status = 'Winner: ' + winner;
@@ -54,14 +68,14 @@ class Game extends React.Component {
         return (
             <div className='game'>
                 <div className='game-board'>
+                    <div style={{ paddingBottom: "10px" }}>{status}</div>
                     <Board
                         squares={current.squares}
                         onClick={(i) => this.handleClick(i)}
                     />
                 </div>
                 <div className='game-info'>
-                    <div>{status}</div>
-                    <div>{/*TODO*/}</div>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         )
